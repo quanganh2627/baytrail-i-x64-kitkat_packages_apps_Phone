3fd6bae585a5a98003abe37c7c8c21e01e428a1f
Alagnou, GregoryX <gregoryx.alagnou@intel.com>
2013-05-31 11:40:53 +0200

    [PORT FROM MAIN] phone: Add sarmanager feature
    BZ: 102083
    
    The SAR manager loads component classes with the
    dex class loader. It is needed to pass the class
    loader to the OemTelephony component in order to
    properly load classes.
    
    Category: Feature Differentiation
    Domain: Telephony-SAR
    Origin: Internal
    Upstream-Candidate: No,proprietary
    
    Change-Id: If543d3293c77365807b0de6d10940ea6c5fc1127
    Orig-Change-Id: I6c00885db57672153956c7e9f88c9142f6eb2115
    Signed-off-by: Alexandre d'Alton <alexandre.d'alton@intel.com>
    Signed-off-by: Alagnou, GregoryX <gregoryx.alagnou@intel.com>
    Reviewed-on: http://android.intel.com:8080/124736
    Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -490,6 +490,32 @@ public class PhoneGlobals extends ContextWrapper
                 Log.e(LOG_TAG, "OEM Hook class creation Failed!");
             }
 
+            try {
+                if (getResources().getBoolean(R.bool.config_usage_sar_manager_supported)) {
+                    if (DBG) {
+                        Log.d(LOG_TAG, "**********************************"
+                                + " SarManagerCreator loading *****************"
+                                + "*********************************************");
+                    }
+                    DexClassLoader classLoader = new DexClassLoader(
+                            getResources().getString(R.string.config_sar_manager_jar_file),
+                            new ContextWrapper(phone.getContext()).getCacheDir().getAbsolutePath(),
+                            null, ClassLoader.getSystemClassLoader());
+                    classLoader.loadClass( getResources().getString(
+                            R.string.config_sar_manager_class_name)).getConstructor(
+                              DexClassLoader.class).newInstance(classLoader);
+                } else {
+                    if (DBG) Log.d(LOG_TAG, "#######################################"
+                            + " NO SAR Manager ############################################");
+                }
+            } catch (Resources.NotFoundException ex) {
+                Log.e(LOG_TAG, "Resource reading Failed!");
+            } catch (ClassNotFoundException ex) {
+                Log.e(LOG_TAG, "SAR Manager class loading failed");
+            } catch (Exception ex) {
+                Log.e(LOG_TAG, "SAR Manager class creation Failed!");
+            }
+
             mHandler.sendEmptyMessage(EVENT_START_SIP_SERVICE);
 
             int phoneType = phone.getPhoneType();
